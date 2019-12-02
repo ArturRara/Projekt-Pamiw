77f8c0fd1c28a976877d5b4b003fd2a306d37d5e
gintera <01131216@pw.edu.pl>
2019-12-02 07:55:15 +0100

    Dno i metr mułu

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from jwt import encode, decode
 from flask_sqlalchemy import SQLAlchemy
 from flask_restplus import Resource
-import datetime
-from functools import wraps
-from flask_jwt_extended import JWTManager
+
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
                                 get_jwt_identity, get_raw_jwt)
-import json
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
@@ -35,8 +32,11 @@ class User(db.Model):
     password = db.Column(db.String(50))
     email = db.Column(db.String(50), unique=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.login
+    def __init__(self, login, password, email):
+        self.login = login
+        self.password = password
+        self.aemailge = email
+
 
 
 def allowed_file(filename):
@@ -66,33 +66,25 @@ def download(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 
-@app.route('/create_user', methods=['POST'])
-def create_user():
-    user = json.loads(request.data)
-    login = user['login']
-    password = user['password']
-    email = user['email']
+@app.route('/registration', methods=['POST'])
+def registration():
+    userLogin = request.form['login']
+    password = request.form['password']
+    email = request.form['email']
 
-    try:
-        new_user = User(login=login, password=generate_password_hash(password, method='sha256'), email=email)
-        db.session.add(new_user)
-        db.session.commit()
-        access_token = create_access_token(identity=User['name'])
-        return {
-            'access_token': access_token
+    new_user = User(login=userLogin, password=generate_password_hash(password, method='sha256'), email=email)
+    db.session.add(new_user)
+    db.session.commit()
 
-        }
-    except:
-        return redirect(url_for('app.register'))
+    return redirect(url_for('login'))
 
 
-@app.route('/login_user', methods=['POST'])
-def login_user():
-    user = json.loads(request.data)
-    login = user['login']
-    password = user['password']
+@app.route('/login', methods=['POST'])
+def login():
+    userLogin = request.form['login']
+    password = request.form['password']
 
-    user = User.query.filter_by(login=login).first()
+    user = User.query.filter_by(login=userLogin).first()
 
     if not user or not check_password_hash(user.password, password):
         return redirect(url_for('app.login'))
@@ -121,6 +113,7 @@ def app_register():
 
 
 @app.route('/upload')
+#@jwt_required
 def app_upload():
     return render_template("uploadPage.html")
 
@@ -138,4 +131,5 @@ class UserLogout(Resource):
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
