5a26f4aedbf9891515b70eba1d7fa52163cf6c6c
ArturRara <01131216@pw.edu.pl>
2019-12-08 23:08:25 -0800

    All work and no play makes jack a dull boy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 from flask import Flask, jsonify, make_response, request, render_template, url_for, redirect, send_from_directory
 from flask_swagger_ui import get_swaggerui_blueprint
 from jwt import encode, decode
@@ -6,38 +7,39 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restplus import Resource
 
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required,
-                                get_jwt_identity, get_raw_jwt)
+                                get_jwt_identity, get_raw_jwt, set_access_cookies, JWTManager, unset_jwt_cookies)
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = "secret"
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user="name", pw="password", url="localhost", db="users")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = {'pdf'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-# @app.before_first_request
-# def create_tables():
-#    db.create_all()
-
-
 class User(db.Model):
+    _tablename_ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(50))
-    email = db.Column(db.String(50), unique=True)
+    login = db.Column(db.String(250), unique=True)
+    password = db.Column(db.String(200))
+    email = db.Column(db.String(500), unique=True)
+
+
 
     def __init__(self, login, password, email):
         self.login = login
         self.password = password
-        self.aemailge = email
+        self.email = email
 
 
+jwt = JWTManager(app)
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -71,12 +73,12 @@ def registration():
     userLogin = request.form['login']
     password = request.form['password']
     email = request.form['email']
-
-    new_user = User(login=userLogin, password=generate_password_hash(password, method='sha256'), email=email)
+#password=generate_password_hash(password, method='sha256')
+    new_user = User(login=userLogin, password=password, email=email)
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('login'))
+    return render_template("loginPage.html")
 
 
 @app.route('/login', methods=['POST'])
@@ -85,15 +87,14 @@ def login():
     password = request.form['password']
 
     user = User.query.filter_by(login=userLogin).first()
+    #check_password_hash()
+    if not user or not user.password == password:
+        render_template("loginPage.html")
 
-    if not user or not check_password_hash(user.password, password):
-        return redirect(url_for('app.login'))
-
-    access_token = create_access_token(identity=User['username'])
-    return {
-        'access_token': access_token
-        # redirect(url_for('app.loginPage'))
-    }
+    access_token = create_access_token(identity=user)
+    resp = make_response(redirect(url_for('main.index')))
+    set_access_cookies(resp, access_token)
+    return resp
 
 
 
@@ -112,8 +113,8 @@ def app_register():
     return render_template("registrationPage.html")
 
 
+# @jwt_required
 @app.route('/upload')
-#@jwt_required
 def app_upload():
     return render_template("uploadPage.html")
 
@@ -121,15 +122,12 @@ def app_upload():
 class UserLogout(Resource):
     @jwt_required
     def post(self):
-        jti = get_raw_jwt()['jti']
-        try:
-            # revoked_token = RevokedTokenModel(jti=jti)
-            # revoked_token.add()
-            return {'message': 'Access token has been revoked'}
-        except:
-            return {'message': 'Something went wrong'}, 500
+        resp = make_response(redirect(url_for('.signin')))
+        unset_jwt_cookies(resp)
+        return resp
 
 
 if __name__ == '__main__':
     db.create_all()
+    app.debug = True
     app.run()
