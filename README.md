543152c71ce93ac50e9b450548211d518113d256
gintera <01131216@pw.edu.pl>
2020-01-13 08:30:19 +0100

    Małymi krokami

--- README.md
+++
@@ -1,6 +0,0 @@
-# Programowanie aplikacji mobilnych i webowych
-
-## Uruchomienie
-docker-compose run --service-ports flask-app
-Docker-compose dziala i powinien dawac mozliwosc wyswietlania, ale wymaga podmiany SQLALCHEMY_DATABASE_URI na DB_URL.
-Powinno wyswietlac, ale wyskakuja bledy o odswiezaniu serwra i raz dziala, a raz nie.
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 import psycopg2
-from flask import Flask, make_response, request, render_template, url_for, redirect, send_from_directory, flash
+from flask import Flask, make_response, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import (create_access_token, jwt_required, set_access_cookies, JWTManager, unset_jwt_cookies)
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -15,11 +15,11 @@ app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes=5)
 jwt = JWTManager(app)
-
 app.config['SECRET_KEY'] = "secret"
-DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user="postgres", pw="password", url="postgres",
-                                                               db="users")
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lvfufmkfbedubw:a844463ec39a52848956642ee9bb431d1b5439ef6e6a38cc6715721da4ba5787@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/dciikefmk2ajh3"
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user="postgres", pw="postgres", url="localhost:5433",
+                                                               db="postgres")
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://lvfufmkfbedubw:a844463ec39a52848956642ee9bb431d1b5439ef6e6a38cc6715721da4ba5787@ec2-54-247-72-30.eu-west-1.compute.amazonaws.com:5432/dciikefmk2ajh3"
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -57,85 +57,60 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/api/upload', methods=['GET', 'POST'])
-@jwt_required
-def upload():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            print('No file attached in request')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            print('No file selected')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return render_template('uploadPage.html')
-
-
-@jwt_required
-def download(filename):
-    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
-
-
-@app.route('/registration', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def registration():
-    login = request.form['login']
-    password = request.form['password']
-    email = request.form['email']
+    login = request.get_json()['login']
+    password = request.get_json()['password']
+    email = request.get_json()['email']
     usedLogin = User.query.filter_by(login=login).first()
     if usedLogin:
-        flash("That username is already taken, please choose another")
-        return redirect(url_for('registration'))
+        return jsonify({"error": "Invalid email"})
     usedEmail = User.query.filter_by(login=login).first()
     if usedEmail:
-        flash("That email is already used")
-        return redirect(url_for('registration'))
+        return jsonify({"error": "Invalid email"})
     password = generate_password_hash(password, method='sha256')
     new_user = User(login=login, password=password, email=email)
     db.session.add(new_user)
     db.session.commit()
 
-    return render_template("loginPage.html")
+    result = {
+        'login': login,
+        'email': email,
+        'password': password
+    }
+
+    return jsonify({'result': result})
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/', methods=['POST'])
 def login():
-    login = request.form['login']
-    password = request.form['password']
+    login = request.get_json()['login']
+    password = request.get_json()['password']
 
     user = User.query.filter_by(login=login).first()
     if not user or not check_password_hash(user.password, password):
-        flash("Login or password is incorrect")
-        return render_template("loginPage.html")
+        return jsonify({"error": "Invalid login and password"})
     else:
-        access_token = create_access_token(identity=login)
-        resp = make_response(redirect(url_for('upload')))
-        set_access_cookies(resp, access_token)
-        return resp
+        access_token = create_access_token(identity={'login': user.login, 'email': user.email})
+        return access_token
 
 
-@app.route('/')
-def app_default():
-    return render_template("loginPage.html")
+@app.route('/upload', methods=['POST'])
+def upload():
+    upload_dir = 'upload_files'
+    fn = ""
+    file_names = []
+    for key in request.files:
+        file = request.files[key]
+        fn = secure_filename(file.filename)
+        file_names.append(fn)
+        print('filename: ', fn)
+        try:
+            file.save(os.path.join(upload_dir, fn))
+        except:
+            print('save fail: ' + os.path.join(upload_dir, fn))
 
-
-@app.route('/loginPage')
-def app_login():
-    return make_response(render_template('loginPage.html'))
-
-
-@app.route('/registration/')
-def app_register():
-    return render_template("registrationPage.html")
-
-
-@jwt_required
-@app.route('/api/upload')
-def app_upload():
-    return render_template("uploadPage.html")
+   # return json.dumps({'filename': [f for f in file_names]})
 
 
 @jwt_required
@@ -148,5 +123,4 @@ def logout():
 
 if __name__ == '__main__':
     db.create_all()
-    app.debug = True
-    app.run()
+    app.run(debug = True)
